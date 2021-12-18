2d9119b5272969b00655f1bb508764c4fd82a593
Valerchixx <95089858+Valerchixx@users.noreply.github.com>
2021-12-18 21:35:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+async function getResponce(){
+    let responce =  await fetch('https://ec2-18-184-156-205.eu-central-1.compute.amazonaws.com/')
+    let content = await responce.json()
+
+    let pack = document.querySelector('.pack');
+    
+        /*pack.innerHTML +=`
+        <div class="title">
+            <h2 class="name">${content.name}</h2>
+            <p class="desc">${content.description}</p>
+            </div>
+            <div class="info">
+                <div class="basic_info">
+                    <div class="installing">
+                    <img  class="icon" src="images/icons/icon-installing.png" alt="icon">
+                    <p class=" parag install">${content.downloads}</p>
+                    </div>
+                    <div class="data-time">
+                    <img class="icon" src="images/icons/icon-clock.png" alt="icon">
+                    <p class=" parag data">${content.date} </p>
+                    </div>
+                </div>
+                <div class="btn_pack">
+                    <a href="${content.link}" class="btn" target="_blank">Go to package</a>
+                </div>
+            </div>
+        `*/
+        if(content.result == null){
+            pack.innerHTML += `
+            <div class="title">
+                <h2 class="name">An error has occurred</h2>
+                <p class="desc">Sorry, the package has not been uploaded</p>
+            </div>
+             `
+        } else{
+        pack.innerHTML +=`
+        <div class="title">
+            <h2 class="name">${content.name}</h2>
+            <p class="desc">${content.description}</p>
+            </div>
+            <div class="info">
+                <div class="basic_info">
+                    <div class="installing">
+                    <img  class="icon" src="images/icons/icon-installing.png" alt="icon">
+                    <p class=" parag install">${content.downloads}</p>
+                    </div>
+                    <div class="data-time">
+                    <img class="icon" src="images/icons/icon-clock.png" alt="icon">
+                    <p class=" parag data">${content.date} </p>
+                    </div>
+                </div>
+                <div class="btn_pack">
+                    <a href="${content.link}" class="btn" target="_blank">Go to package</a>
+                </div>
+            </div>
+        `
+        }
+    }
+
+getResponce()
+
+
+
+
+           
